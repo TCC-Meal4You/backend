5210cf912228f0b18391dbf94e9078546f56bedc
henrique-castilho <henrique.castilho.pires@gmail.com>
2025-09-07 03:41:46 -0300

    refactor: refatora controllers, services e entities para melhorar consistencia

--- src/main/java/com/api/meal4you/service/AdmRestauranteService.java
+++ src/main/java/com/api/meal4you/service/AdmRestauranteService.java
@@ -2,46 +2,46 @@ package com.api.meal4you.service;
 
 import com.api.meal4you.entity.AdmRestaurante;
 import com.api.meal4you.repository.AdmRestauranteRepository;
+
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class AdmRestauranteService {
-    private AdmRestauranteRepository repository;
+    private final AdmRestauranteRepository admRepository;
 
-    public AdmRestauranteService(AdmRestauranteRepository repository) {
-        this.repository = repository;
-    }
-
-    public void salvarUsuario(AdmRestaurante admRestaurante) {
-        repository.saveAndFlush(admRestaurante);
+    public void cadastararAdm(AdmRestaurante admRestaurante) {
+        admRepository.saveAndFlush(admRestaurante);
     }
 
     public Optional<AdmRestaurante> buscarPorEmail(String email) {
-        return repository.findByEmail(email);
+        return admRepository.findByEmail(email);
     }
 
     public void atualizarPorId(int id, AdmRestaurante admRestaurante) {
-        AdmRestaurante admEntity = repository.findById(id)
+        AdmRestaurante admEntity = admRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Administrador de restaurante não encontrado"));
 
         AdmRestaurante admAtualizado = AdmRestaurante.builder()
-                .id(admEntity.getId())
+                .id_admin(admEntity.getId_admin())
                 .email(admRestaurante.getEmail() != null ? admRestaurante.getEmail() : admEntity.getEmail())
                 .nome(admRestaurante.getNome() != null ? admRestaurante.getNome() : admEntity.getNome())
                 .senha(admRestaurante.getSenha() != null ? admRestaurante.getSenha() : admEntity.getSenha())
                 .build();
 
-        repository.saveAndFlush(admAtualizado);
+        admRepository.saveAndFlush(admAtualizado);
     }
 
     public void deletarPorEmail(String email, String senha) {
-        AdmRestaurante admRestaurante = repository.findByEmail(email)
+        AdmRestaurante admRestaurante = admRepository.findByEmail(email)
                 .orElseThrow(() -> new RuntimeException("Email não encontrado"));
         if (!admRestaurante.getSenha().equals(senha)) {
             throw new RuntimeException("Senha incorreta");
         }
-        repository.deleteByEmail(email);
+        admRepository.deleteByEmail(email);
     }
 }
