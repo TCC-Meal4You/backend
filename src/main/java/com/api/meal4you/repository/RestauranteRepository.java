5b02e8748a8aa0b14966e70b5c63406be52e02ff
henrique-castilho <henrique.castilho.pires@gmail.com>
2025-09-07 03:42:40 -0300

    feat(restaurante): adiciona CRUD completo para restaurante

---
+++ src/main/java/com/api/meal4you/repository/RestauranteRepository.java
@@ -0,0 +1,12 @@
+package com.api.meal4you.repository;
+
+import com.api.meal4you.entity.Restaurante;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RestauranteRepository extends JpaRepository<Restaurante, Integer> {
+    Optional<Restaurante> findByNomeAndLocalizacao(String nome, String localizacao);
+
+}
