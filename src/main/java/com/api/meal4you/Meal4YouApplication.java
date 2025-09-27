bac41f12f50aff5faa0093966fe1475f071b275c
henrique-castilho <henrique.castilho.pires@gmail.com>
2025-09-27 18:00:33 -0300

    feat(security): liberar acesso ao Swagger sem autenticação

--- src/main/java/com/api/meal4you/Meal4YouApplication.java
+++ src/main/java/com/api/meal4you/Meal4YouApplication.java
@@ -2,13 +2,11 @@ package com.api.meal4you;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class Meal4YouApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Meal4YouApplication.class, args);
 	}
-
 }
