d5536527da1c54be9660451b47bd3a6956431705
Felipe <felipenn27@gmail.com>
2025-10-08 17:21:21 -0300

    refactor(entity): reestrutura a entity refeicao, cria entities auxiliares da refeicao e padroniza formatacao das entities no geral

--- src/main/java/com/api/meal4you/entity/Restricao.java
+++ src/main/java/com/api/meal4you/entity/Restricao.java
@@ -1,12 +1,15 @@
 package com.api.meal4you.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.MapsId;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,85 +23,21 @@ import lombok.Setter;
 @Builder
 @Entity
 @Table(name = "restricao")
-
 public class Restricao {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idRestricao;
 
-    @OneToOne
-    @MapsId
-    @JoinColumn(name = "id_ingrediente", referencedColumnName = "id_ingrediente", nullable = false, unique = true)
-    private Ingrediente ingrediente;
+    @NotBlank
+    @Column(length = 100, unique = true, nullable = false)
+    private String tipo;
 
-    @Column(name = "com_gluten", nullable = false)
-    private boolean comGluten;
+    // Relacionamento com usuarios através da tabela intermediária
+    @OneToMany(mappedBy = "restricao")
+    private List<UsuarioRestricao> usuarios;
 
-    @Column(name = "com_lactose", nullable = false)
-    private boolean comLactose;
-
-    @Column(name = "com_acucar", nullable = false)
-    private boolean comAcucar;
-
-    @Column(name = "com_ovos", nullable = false)
-    private boolean comOvos;
-
-    @Column(name = "com_soja", nullable = false)
-    private boolean comSoja;
-
-    @Column(name = "com_nozes", nullable = false)
-    private boolean comNozes;
-
-    @Column(name = "com_amendoim", nullable = false)
-    private boolean comAmendoim;
-
-    @Column(name = "com_frutos_do_mar", nullable = false)
-    private boolean comFrutosDoMar;
-
-    @Column(name = "com_milho", nullable = false)
-    private boolean comMilho;
-
-    @Column(name = "com_conservantes", nullable = false)
-    private boolean comConservantes;
-
-    @Column(name = "com_corantes", nullable = false)
-    private boolean comCorantes;
-
-    @Column(name = "com_cafeina", nullable = false)
-    private boolean comCafeina;
-
-    @Column(name = "com_alcool", nullable = false)
-    private boolean comAlcool;
-
-    @Column(name = "com_oleo_de_palma", nullable = false)
-    private boolean comOleoDePalma;
-
-    @Column(name = "com_gordura_trans", nullable = false)
-    private boolean comGorduraTrans;
-
-    @Column(name = "com_adocante_artificial", nullable = false)
-    private boolean comAdocanteArtificial;
-
-    @Column(name = "com_aromatizante_artificial", nullable = false)
-    private boolean comAromatizanteArtificial;
-
-    @Column(name = "com_trigo", nullable = false)
-    private boolean comTrigo;
-
-    @Column(name = "com_ingredientes_geneticamente_modificados", nullable = false)
-    private boolean comIngredientesGeneticamenteModificados;
-
-    @Column(name = "com_sal", nullable = false)
-    private boolean comSal;
-
-    @Column(name = "com_pimenta", nullable = false)
-    private boolean comPimenta;
-
-    @Column(name = "com_fibras", nullable = false)
-    private boolean comFibras;
-
-    @Column(name = "com_fosfato", nullable = false)
-    private boolean comFosfato;
-
-    @Column(name = "com_fermento", nullable = false)
-    private boolean comFermento;
+    // Relacionamento com ingredientes através da tabela intermediária
+    @OneToMany(mappedBy = "restricao")
+    private List<IngredienteRestricao> ingredientes;
 }
