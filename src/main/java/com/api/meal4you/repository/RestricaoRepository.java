837603bec936fdc6d260e09e0e395783ab38044e
henrique-castilho <henrique.castilho.pires@gmail.com>
2025-10-09 23:54:13 -0300

    feat(restricao): implementa agendamento de sincronização com IA e endpoints de restrição

---
+++ src/main/java/com/api/meal4you/repository/RestricaoRepository.java
@@ -0,0 +1,9 @@
+package com.api.meal4you.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.meal4you.entity.Restricao;
+
+public interface RestricaoRepository extends JpaRepository<Restricao, Integer> {
+
+}
