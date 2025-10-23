244ffcad31568a2dceb4f8b771275d449558bd46
thediaax <gabrieldias23.dev@gmail.com>
2025-10-23 00:00:40 -0300

    feat(Ingrediente): adiciona mapper e repository do ingrediente

---
+++ src/main/java/com/api/meal4you/mapper/IngredienteMapper.java
@@ -0,0 +1,28 @@
+package com.api.meal4you.mapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import com.api.meal4you.dto.IngredienteResponseDTO;
+import com.api.meal4you.entity.Ingrediente;
+import com.api.meal4you.entity.IngredienteRestricao;
+
+public class IngredienteMapper {
+
+    public static IngredienteResponseDTO toResponse(Ingrediente ingrediente) {
+        return IngredienteResponseDTO.builder()
+                .idIngrediente(ingrediente.getIdIngrediente())
+                .nome(ingrediente.getNome())
+                .IdAdmin(ingrediente.getAdmin().getIdAdmin())
+                .restricoes(ingrediente.getRestricoes().stream()
+                        .map(IngredienteRestricao::getRestricao)
+                        .map(RestricaoMapper::toResponse)
+                        .collect(Collectors.toList()))
+                .build();
+    }
+
+    public static List<IngredienteResponseDTO> toResponseList(List<Ingrediente> ingredientes){
+        return ingredientes.stream()
+                .map(IngredienteMapper::toResponse)
+                .collect(Collectors.toList());
+    }
+}
