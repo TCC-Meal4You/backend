67cf0186ec07f2b27e977b2e89d3eec46cfabf72
henrique-castilho <henrique.castilho.pires@gmail.com>
2025-10-30 21:40:23 -0300

    feat(email): migrar envio de e-mails de JavaMailSender para SendGrid

--- src/main/java/com/api/meal4you/service/EmailCodeSenderService.java
+++ src/main/java/com/api/meal4you/service/EmailCodeSenderService.java
@@ -1,25 +1,47 @@
 package com.api.meal4you.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import com.sendgrid.*;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
 
 @Service
 public class EmailCodeSenderService {
 
-    @Autowired
-    private JavaMailSender mailSender;
+    @Value("${sendgrid.api.key}")
+    private String sendGridApiKey;
 
-    public void enviarEmail(String toEmail,
-                          String subject,
-                          String body) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("meal4you.co@gmail.com");
-        message.setTo(toEmail);
-        message.setSubject(subject);
-        message.setText(body);
-        mailSender.send(message);
+    public void enviarEmail(String toEmail, String subject, String body) {
+        Email from = new Email("meal4you.co@gmail.com");
+        Email to = new Email(toEmail);
+        Content content = new Content("text/plain", body);
+        Mail mail = new Mail(from, subject, to, content);
 
+        SendGrid sg = new SendGrid(sendGridApiKey);
+        Request request = new Request();
+
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            sg.api(request);
+        } catch (IOException ex) {
+            throw new ResponseStatusException(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "Erro ao enviar e-mail via SendGrid: " + ex.getMessage()
+            );
+        } catch (Exception ex) {
+            throw new ResponseStatusException(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "Erro inesperado ao enviar e-mail: " + ex.getMessage()
+            );
+        }
     }
 }
